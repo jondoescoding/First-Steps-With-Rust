3d361cf9d67aa72cec6bebf96de3a1c33fa10727
jondoescoding <jondidathing.jm@gmail.com>
2024-03-29 14:41:49 -0500

    treehouse_enum example uploaded

---
+++ treehouse_guestlist_enum/src/main.rs
@@ -0,0 +1,68 @@
+use std::io::stdin;
+
+#[derive(Debug)]
+struct Visitor{
+    name: String,
+    greeting: String,
+}
+
+#[derive(Debug)]
+enum VisitorAction {
+    Accept,
+    AcceptWithNote { note: String},
+    Refuse,
+    Probation,
+}
+
+fn what_is_your_name() -> String {
+    let mut your_name = String::new();
+    stdin()
+        .read_line(&mut your_name)
+        .expect("Failed to read line");
+    return your_name
+        .trim()
+        .to_lowercase()
+}
+
+impl Visitor {
+    fn new (name: &str, greeting: &str) -> Self {
+        Self {
+            name: name.to_lowercase(),
+            greeting: greeting.to_lowercase(),
+        }
+    }
+    fn greet_visitor(&self){
+        println!("{}", self.greeting);
+    }
+}
+
+fn main() {
+    let mut visitor_list = vec![
+        Visitor::new("Bert", "Peace be with you"),
+        Visitor::new("Steve", "Shalom, Steve"),
+        Visitor::new("Fred", "Fred is the man")
+    ];
+
+    'main_loop: loop { // Labeled loop
+        println!("Hello, what is your name? ");
+        let name = what_is_your_name();
+
+        let known_visitor = visitor_list
+            .iter()
+            .find(|visitor| visitor.name == name);
+    
+        match known_visitor {
+            Some(visitor) => visitor.greet_visitor(),
+            None => {
+                if name.is_empty(){
+                    break 'main_loop;
+                } else {
+                    println!("{} is not on the visitor list", name);
+                    visitor_list.push(Visitor::new(&name, "Welcome new friend"));
+                }
+            }
+        }
+    }
+
+    println!("Current list of visitors: {:#?}", visitor_list)
+}
